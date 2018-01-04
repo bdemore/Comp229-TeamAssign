f02789266ed503b0369276904b8638b0732b4585
Bruno <brunodemore@hotmail.com>
2018-01-04 00:19:56 -0500

    Book Details Page base

--- Comp229-TeamAssign/BookDetail.aspx
+++ Comp229-TeamAssign/BookDetail.aspx
@@ -1,3 +1,62 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BookDetail.aspx.cs" Inherits="Comp229_TeamAssign.BookDetail" %>
+
 <asp:Content ID="BookDetailContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <%-- Error Panel --%>
+    <asp:Panel ID="ErrorPanel" runat="server" CssClass="register-error-message-hidden">
+        <div><%= message %></div>
+    </asp:Panel>
+
+    <div class="col-sm-12">
+        <div class="book-image-container col-sm-6">
+            Image
+        </div>
+        <div class="book-detail-container col-sm-6">
+            <div>
+                Title: <%= book.Title %>
+            </div>
+            <div>
+                Authors: <%= book.Authors %>
+            </div>
+            <div>
+                Category: <%= book.Categories %>
+            </div>
+            <div>
+                Description <%= book.Description %>
+            </div>
+            <div>
+                Publication Date: <%= book.PublicationDate %>
+            </div>
+            <div>
+                Edition: <%= book.Edition %>
+            </div>
+            <div>
+                Availability: <%= book.IsAvailable %>
+            </div>
+            <div>
+                Quantity Available: <%= book.QuantityAvailable %>
+            </div>
+            <div>
+                Publisher: <%= book.Publisher %>
+            </div>
+        </div>
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+            thumbnail
+        </div>
+        <div class="col-sm-6">
+            <% if (null != Session["LoggedUser"])
+                { %>
+            <div class="ub-book-button-holder">
+                <asp:Button
+                    ID="ReserveButton"
+                    Text="Reserve"
+                    CommandArgument='<%# Eval("PrimaryKey.Key") %>'
+                    CssClass="ub-book-button"
+                    OnClick="ReserveButton_Click"
+                    runat="server" />
+            </div>
+            <% } %>
+        </div>
+    </div>
 </asp:Content>
--- Comp229-TeamAssign/BookDetail.aspx.cs
+++ Comp229-TeamAssign/BookDetail.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Comp229_TeamAssign.Controllers;
+using Comp229_TeamAssign.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,55 @@ namespace Comp229_TeamAssign
 {
     public partial class BookDetail : System.Web.UI.Page
     {
+        // The book controller to be used.
+        private IBookController bookController = BookController.GetInstance();
+        private IBookRentalController bookRentalController = BookRentalController.GetInstance();
+
+        // The error message.
+        protected string message = "";
+
+        protected Book book;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                book = bookController.RetrieveBookDetails(Request.QueryString["isbn"], Session["BookList"] as List<Book>);       
+            }
 
+            //TODO
+            //if (null == Session["BookList"])
+            //{
+            //    Response.Redirect("~/");
+            //}
+        }
+
+        protected void ReserveButton_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            User user = Session["LoggedUser"] as User;
+            List<Book> books = Session["BookList"] as List<Book>;
+            BookRental bookRental = bookRentalController.ReserveBook(user, books.Find(tBook => tBook.PrimaryKey.Key == decimal.Parse(button.CommandArgument)));
+
+            if (null == bookRental)
+            {
+                ShowErrorMessage(string.Format("{0}, you have a pending rental. Please, return the book(s) in your possession before renting again!", user.FirstName));
+            }
+            else
+            {
+                Session["BookRental"] = bookRental;
+                Response.Redirect("~/ReserveDetails");
+            }
+        }
+
+        ///// <summary>
+        ///// Shows to the user any unexpecte error that may occur during the database communication.
+        ///// </summary>
+        ///// <param name="message">The error message to be shown.</param>
+        private void ShowErrorMessage(string message)
+        {
+            this.message = string.Format("<hr/><b>{0}</b><hr/>", message);
+            ErrorPanel.CssClass = "register-error-message";
         }
     }
 }
